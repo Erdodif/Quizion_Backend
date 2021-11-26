78f82a4cd7c795151c32245e962b4984f2532fc7
Nagy Bendegúz <bendeguz@medea.hu>
2021-11-26 19:18:49 +0100

    routes get quizes

--- src/routes.php
+++ src/routes.php
@@ -1,2 +1,14 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Quizion\Backend\Quiz;
+
+return function(Slim\App $app) {
+    $app->get("/quizes", function(Request $request, Response $response) {
+        $quizes = Quiz::all();
+        $out = $quizes->toJson();
+        $response->getBody()->write($out);
+        return $response->withHeader("Content-Type", "application/json");
+    });
+};
