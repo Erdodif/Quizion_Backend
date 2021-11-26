80e3219b7571c317072deddbec4d26703079c0c0
Nagy Bendegúz <bendeguz@medea.hu>
2021-11-26 20:10:06 +0100

    routes folytatása

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,8 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Quizion\Backend\Answer;
+use Quizion\Backend\Question;
 use Quizion\Backend\Quiz;
 
 return function(Slim\App $app) {
@@ -10,5 +12,29 @@ return function(Slim\App $app) {
         $out = $quizes->toJson();
         $response->getBody()->write($out);
         return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get("/questions", function(Request $request, Response $response) {
+        $question = Question::all();
+        $out = $question->toJson();
+        $response->getBody()->write($out);
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+
+
+
+
+    
+    $app->get("/question/{id}", function(Request $request, Response $response, array $args) {
+        $question = Question::find($args["id"]);
+        $response->getBody()->write($question->toJson());
+        return $response->withHeader("Content-Type", "application/json")->withStatus(201);
+    });
+
+    $app->get("/answers/{id}", function(Request $request, Response $response, array $args) {
+        $answer = Answer::find($args["id"]);
+        $response->getBody()->write($answer->toJson());
+        return $response->withHeader("Content-Type", "application/json")->withStatus(201);
     });
 };
