235d9b43b2c1a4ad4cb6ef6005946ba27226ef5b
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-11-26 20:07:11 +0100

    get answers

--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,19 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Quizion\Backend\Quiz;
+use Quizion\Backend\Answer;
 
 return function(Slim\App $app) {
     $app->get("/quizes", function(Request $request, Response $response) {
         $quizes = Quiz::all();
         $out = $quizes->toJson();
+        $response->getBody()->write($out);
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get("/answers", function(Request $request, Response $response) {
+        $answers = Answer::all();
+        $out = $answers->toJson();
         $response->getBody()->write($out);
         return $response->withHeader("Content-Type", "application/json");
     });
