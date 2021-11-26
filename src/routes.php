86fc79b88eea1b3d23392f976bf2581012dd4507
Nagy Bendegúz <bendeguz@medea.hu>
2021-11-26 20:29:12 +0100

    route javítás

--- src/routes.php
+++ src/routes.php
@@ -7,20 +7,19 @@ use Quizion\Backend\Question;
 use Quizion\Backend\Answer;
 
 return function(Slim\App $app) {
+    // GET quizes questions answers
     $app->get("/quizes", function(Request $request, Response $response) {
         $quizes = Quiz::all();
         $out = $quizes->toJson();
         $response->getBody()->write($out);
         return $response->withHeader("Content-Type", "application/json");
     });
-
     $app->get("/questions", function(Request $request, Response $response) {
         $question = Question::all();
         $out = $question->toJson();
         $response->getBody()->write($out);
         return $response->withHeader("Content-Type", "application/json");
     });
-
     $app->get("/answers", function(Request $request, Response $response) {
         $answers = Answer::all();
         $out = $answers->toJson();
@@ -28,19 +27,24 @@ return function(Slim\App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
+    // GET ID quizes questions answers
+    $app->get("/quiz/{id}", function(Request $request, Response $response, array $args) {
+        $quiz = Quiz::find($args["id"]);
+        $response->getBody()->write($quiz->toJson());
+        return $response->withHeader("Content-Type", "application/json")->withStatus(201);
+    });
     $app->get("/question/{id}", function(Request $request, Response $response, array $args) {
         $question = Question::find($args["id"]);
         $response->getBody()->write($question->toJson());
         return $response->withHeader("Content-Type", "application/json")->withStatus(201);
     });
-
     $app->get("/answers/{id}", function(Request $request, Response $response, array $args) {
         $answer = Answer::find($args["id"]);
         $response->getBody()->write($answer->toJson());
         return $response->withHeader("Content-Type", "application/json")->withStatus(201);
     });
 
-    //POST quizes
+    // POST quizes questions answers
     $app->post("/quizes", function(Request $request, Response $response) {
         $input = json_decode($request->getBody(), true);
         $quiz = Quiz::create($input);
@@ -49,17 +53,14 @@ return function(Slim\App $app) {
         $response->getBody()->write($kimenet);
         return $response->withStatus(201)->withHeader("Content-Type", "application/json");
     });
-
-    //POST questions
     $app->post("/questions", function(Request $request, Response $response) {
         $input = json_decode($request->getBody(), true);
         $question = Question::create($input);
         $question->save();
-        $kimenet = $quiz->toJson();
+        $kimenet = $question->toJson();
         $response->getBody()->write($kimenet);
         return $response->withStatus(201)->withHeader("Content-Type", "application/json");
     });
-    //POST answers
     $app->post("/answers", function(Request $request, Response $response) {
         $input = json_decode($request->getBody(), true);
         $quiz = Quiz::create($input);
@@ -68,5 +69,4 @@ return function(Slim\App $app) {
         $response->getBody()->write($kimenet);
         return $response->withStatus(201)->withHeader("Content-Type", "application/json");
     });
-
 };
