f4234727b1c566a0b337afa22782cfa3f4bf9520
Nagy Bendegúz <bendeguz@medea.hu>
2021-11-26 18:57:21 +0100

    index.php hozzáadása

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require "vendor/autoload.php";
+use Illuminate\Database\Capsule\Manager;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$dbManager = new Manager();
+$dbManager->addConnection([
+    "driver" => "mysql",
+    "host" => "localhost",
+    "database" => "quizion",
+    "username" => "root",
+    "password" => "",
+    "charset" => "utf8mb4",
+    "collation" => "utf8mb4_unicode_ci",
+    "prefix" => "",
+]);
+
+$dbManager->setAsGlobal();
+$dbManager->bootEloquent();
+
+$routes = require "src/routes.php";
+
+$routes($app);
+
+$app->run();
