2e63c567ae8a14b5d4d49c1d92eef65850c11a94
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-11-26 19:02:07 +0100

    Answer.php hozzáadása

---
+++ src/classes/Answer.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Quizion\Backend;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model{
+    protected $table = "answer";
+    public $timestamps = false;
+    protected $guarded = ["id"];
+}
