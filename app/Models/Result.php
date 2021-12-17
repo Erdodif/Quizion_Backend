35cc2268a3c8e6c8e348c1c3ec2c966d9600e2f2
Erdodif <erdodif@gmail.com>
2021-12-17 19:15:28 +0100

    Eredmény lekérdezésének, és mentésének megvalósítása

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Companion\Data;
 use App\Companion\Message;
 use \Error;
+use Exception;
 
 class Result extends Model{
     protected $table = "results";
@@ -33,5 +34,70 @@ class Result extends Model{
         } finally {
             return $data;
         }
+    }
+
+    static function getByQuiz(int $quiz_id):Data
+    {
+        try {
+            $result = Result::where("quiz_id",$quiz_id)->get();
+            if (isset($result[0]["id"])) {
+                $data = new Data(
+                    RESPONSE_OK,
+                    $result
+                );
+            } else {
+                $data = new Data(
+                    ERROR_NOT_FOUND,
+                    new Message("No one played this quiz yet!")
+                );
+            }
+        } catch (Error $e) {
+            $data = new Data(
+                ERROR_INTERNAL,
+                new Message("An internal error occured! " . $e->getMessage())
+            );
+        } finally {
+            return $data;
+        }
+
+    }
+
+    static function saveFromGame(Game $game):Data
+    {   
+        try{
+            $result = Result::firstOrNew(["user_id"=>$game->user_id,"quiz_id"=>$game->quiz_id]);
+            $isnew = empty($result->point);
+            if($isnew){
+                $result->point = $game->right;
+                $result->save();
+                $data = new Data(
+                    RESPONSE_CREATED,
+                    new Message("First result by the user.")
+                );
+            }
+            else{
+                if($result->point > $game->right){ 
+                    $result->point = $game->right;
+                    $result->save();
+                    $data = new Data(
+                        RESPONSE_OK,
+                        new Message("New Highscore!")
+                    );
+                }
+                else{
+                    $data = new Data(
+                        RESPONSE_NOT_MODIFIED,
+                        new Message("Worse than last the time...")
+                    );
+                }
+            }
+        }
+        catch (Exception $e){
+            $data = new Data(
+                ERROR_INTERNAL,
+                new Message("An internal error occured! $e")
+            );
+        }
+        return $data;
     }
 }
