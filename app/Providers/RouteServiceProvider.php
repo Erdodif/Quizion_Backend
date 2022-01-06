5629420d36c1f8d9c6fc9930cf10eaa72bdc2c0a
Nagy Bendegúz <bendeguz@medea.hu>
2022-01-06 21:27:56 +0100

    Web Laravelre átállás.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -66,6 +66,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/resultRoutes.php'));
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
         });
     }
 
