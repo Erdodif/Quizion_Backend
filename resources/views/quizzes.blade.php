1fca33111f4f113082cb0f19b794748e4b39fa44
Bendeguz <bendeguz@medea.hu>
2022-02-22 13:47:01 +0100

    Web - fetch folytatása.

--- resources/views/quizzes.blade.php
+++ resources/views/quizzes.blade.php
@@ -17,72 +17,51 @@
             <form method="POST" action="{{ url('api/play/newgame/' . $quiz->id) }}">
                 @csrf
                 <input type="submit" value="{{ __('Play') }}">
-                <a id="a" onclick="loadDataQuestion()">loadDataQuestion</a>
-                <a id="b" onclick="loadDataAnswers()">loadDataAnswers</a>
-                <p onclick="newGame()">newGame</p>
             </form>
+            <p id="a" onclick="loadDataQuestion(@php echo $quiz->id; @endphp)">loadDataQuestion</p>
+            <p id="b" onclick="loadDataAnswers(@php echo $quiz->id; @endphp)">loadDataAnswers</p>
+            <p onclick="newGame(@php echo $quiz->id; @endphp)">nextQuestion</p>
         </div>
     @endforeach
-        {{--
-            {{ url('api/play/' . $quiz->id . '/question') }}
-            {{ url('api/play/' . $quiz->id . '/answers') }}
-            {{ url('api/play/' . $quiz->id . '/choose') }} name="chosen[]" POST
-
-            quiz/{{ $quiz->id }}/question/1
-        --}}
+    <input type="number" id="pickAnswer">
+        {{--quiz/{{ $quiz->id }}/question/1--}}
     <script>
-        async function loadDataQuestion() {
-            //URL::to('admin/posts/edit/' . $post->id)
-
-            //let url = "{{!! url() }}";
-            //let a = document.getElementById("a");
-            //a.setAttribute("href", "api/play/" + id + "/question");
-
-            //let id = {{ $quiz->id }};
-            //console.log(id);
-
-            let Response = await fetch('{{ url('api/play/' . $quiz->id . '/question') }}');
+        async function loadDataQuestion(id) {
+            //let Response = await fetch({!! '\'' . url('api/play/' . $quiz->id . '/question') . '\'' !!});
+            let Response = await fetch(`http://127.0.0.1:8000/api/play/${id}/question`);
             let data = await Response.json();
             console.log("Question");
             console.log(data);
         }
 
         async function loadDataAnswers(id) {
-            console.log(id);
-            let Response = await fetch('{{ url('api/play/id/answers') }}');
+            //let Response = await fetch({!! '\'' . url('api/play/' . $quiz->id . '/answers') . '\'' !!});
+            let Response = await fetch(`http://127.0.0.1:8000/api/play/${id}/answers`);
             let data = await Response.json();
             console.log("Answers");
             console.log(data);
         }
 
-        /*const data = { "chosen": [1] }; // 1 8
-        fetch('{{ url('api/play/1/choose') }}', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify(data),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });*/
-
-        async function newGame() {
-            let data = await fetch('{{ url('api/play/1/choose') }}', {
+        async function newGame(id) {
+            let answerId = document.getElementById("pickAnswer").value;
+            const data = { "chosen": [answerId] };
+            console.log(data["chosen"]);
+            //let data = await fetch({!! '\'' . url('api/play/1/choose') . '\'' !!}, {
+            await fetch(`http://127.0.0.1:8000/api/play/${id}/choose`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
-                }
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
             });
-            console.log(data);
-            //await this.loadData()
         }
-
     </script>
 @endsection
