91df70d5eca5a8854789668239aaba7b0f9ec6ea
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-03-03 17:59:58 +0100

    UserController index, show, destroy

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Companion\Data;
+use App\Companion\Message;
+use App\Companion\ResponseCodes;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Error;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $result = User::all();
+            if (isset($result[0]["id"])) {
+                return (new Data(
+                    ResponseCodes::RESPONSE_OK,
+                    $result
+                ))->toResponse();
+            } else {
+                return (new Data(
+                    ResponseCodes::ERROR_NOT_FOUND,
+                    new Message("There is no user!")
+                ))->toResponse();
+            }
+        } catch (Error $e) {
+            return (new Data(
+                ResponseCodes::ERROR_INTERNAL,
+                new Message("An internal error occured! " . $e->getMessage())
+            ))->toResponse();
+        } 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, ?int $question_id = null)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(int $user)
+    {
+        return User::getByAny($user)->toResponse();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $answer)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $user)
+    {
+        return User::deleteById($user)->toResponse();
+    }
+}
