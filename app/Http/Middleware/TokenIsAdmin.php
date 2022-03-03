8c29e22b832e650ca758cd2e5e27e792ea7a858c
Erdodif <erdodif@gmail.com>
2022-03-03 18:44:21 +0100

    Admin jogosultságfigyelő middleware létrehozása

---
+++ app/Http/Middleware/TokenIsAdmin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Companion\Data;
+use App\Companion\Message;
+use App\Companion\ResponseCodes;
+use App\Models\Admin;
+use App\Models\Token;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TokenIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    { // Használat csak a TokenIsValid midlleware-el együtt(biztonsági okokból!)
+        $id = $request->userID;
+        if(!Admin::isAdmin($id)){
+            return (new Data(
+                ResponseCodes::ERROR_FORBIDDEN,
+                new Message("You have no permission to perform this action!")
+            ))->toResponse();
+        }
+        return $next($request);
+    }
+}
