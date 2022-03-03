0f6a2200c25e8daf7a9ffd59a73479c03c1930e4
Erdodif <erdodif@gmail.com>
2022-03-03 18:44:39 +0100

    Admin jogosultságfigyelő middleware cimkézése

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth.token' =>\App\Http\Middleware\TokenIsValid::class,
+        'auth.admin' =>\App\Http\Middleware\TokenIsAdmin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
