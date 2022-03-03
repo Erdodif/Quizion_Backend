bb0c28d1e9fb56f14f144d611a82789b31a0f3b4
Nagy Bendegúz <bendeguz@medea.hu>
2022-03-03 20:29:44 +0100

    UserRequest felhasználói adatok validációja.

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'min:5', 'max:25', 'unique:users'],
+            'email' => 'required|email|min:6|max:50|unique:users',
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()],
+        ];
+        //'regex:/^[a-zA-Z]$/u'
+        //'regex:(/^/W|/d$/u)+'
+    }
+}
