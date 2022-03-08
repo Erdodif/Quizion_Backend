1b9789483c549d7c29afc18d8489e9e4b9b8d40a
Erdodif <erdodif@gmail.com>
2022-03-08 16:52:30 +0100

    felhasználói végpontok ideiglenes eltávolítása

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -45,10 +45,6 @@ class RouteServiceProvider extends ServiceProvider
             Route::prefix('api')
                 ->middleware('api')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/userRoutes.php'));
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/gameRoutes.php'));
             Route::prefix('api')
                 ->middleware('api')
