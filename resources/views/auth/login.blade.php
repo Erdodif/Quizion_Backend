938f14fb9e1ede71d7d5359ff84dfb4175f17c2b
Bendeguz <bendeguz@medea.hu>
2022-03-09 14:14:19 +0100

    Web - login és register formok kinézetének javítása.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,19 +7,15 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
         <div>
-            <label for="login">Username or Email</label>
-            <input type="text" id="login" name="login" value="{{ old('login') }}" placeholder="{{ __('Username or Email') }}" required>
+            <input type="text" name="login" value="{{ old('login') }}" placeholder="{{ __('Username or Email') }}" required>
         </div>
         <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" value="" placeholder="{{ __('Password') }}" required>
+            <input type="password" name="password" placeholder="{{ __('Password') }}" required>
         </div>
-        <div>
-            <label id="remember_me_label" for="remember_me">
-                <input id="remember_me" type="checkbox" name="remember">
-                <span>{{ __('Remember me') }}</span>
-            </label>
-        </div>
+        <label id="remember_me_label" for="remember_me">
+            <input id="remember_me" type="checkbox" name="remember">
+            <span>{{ __('Remember me') }}</span>
+        </label>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
                 <div class="error_message">{{ $error }}</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,20 +7,16 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
         <div>
-            <label for="name_id">Username</label>
-            <input type="text" id="name_id" name="name" value="{{ old('name') }}" placeholder="{{ __('Username') }}" required>
+            <input type="text" name="name" value="{{ old('name') }}" placeholder="{{ __('Username') }}" required>
         </div>
         <div>
-            <label for="email_id">Email</label>
-            <input type="email" id="email_id" name="email" value="{{ old('email') }}" placeholder="{{ __('Email') }}" required>
+            <input type="email" name="email" value="{{ old('email') }}" placeholder="{{ __('Email') }}" required>
         </div>
         <div>
-            <label for="password_id">Password</label>
-            <input type="password" id="password_id" name="password" value="" placeholder="{{ __('Password') }}" required>
+            <input type="password" name="password" placeholder="{{ __('Password') }}" required>
         </div>
         <div>
-            <label for="password_confirmation_id">Confirm Password</label>
-            <input type="password" id="password_confirmation_id" name="password_confirmation" placeholder="{{ __('Confirm Password') }}" value="" required>
+            <input type="password" name="password_confirmation" placeholder="{{ __('Confirm Password') }}" required>
         </div>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
@@ -30,8 +26,8 @@
         @if ($userError ?? null)
             <div class="error_message">{{ $userError }}</div>
         @endif
-        <div id="already_registered"><a href="{{ route('login') }}">{{ __('Already registered?') }}</a></div>
         <input id="button_one_click" type="submit" value="{{ __('Register') }}">
         <a class="button" href="{{ route('index') }}">{{ __('Index') }}</a>
+        <a class="button" href="{{ route('login') }}">{{ __('Login') }}</a>
     </form>
 @endsection
