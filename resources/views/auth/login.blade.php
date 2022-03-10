7b310a7156cbdfb2e1b92ac0c76e9573e7c799d6
Bendeguz <bendeguz@medea.hu>
2022-03-10 13:52:19 +0100

    Web - login formok kinézetének javítása.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,17 +12,20 @@
         <div>
             <input type="password" name="password" placeholder="{{ __('Password') }}" required>
         </div>
+        @if ($errors->any())
+            <div id="error_message_margin_remember_me">
+                @foreach ($errors->all() as $error)
+                    <div class="error_message">{{ $error }}</div>
+                @endforeach
+            </div>
+        @endif
         <label id="remember_me_label" for="remember_me">
             <input id="remember_me" type="checkbox" name="remember">
             <span>{{ __('Remember me') }}</span>
         </label>
-        @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                <div class="error_message">{{ $error }}</div>
-            @endforeach
-        @endif
-        <a id="forgot_your_password" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
         <input id="button_one_click" type="submit" value="{{ __('Login') }}">
         <a class="button" href="{{ route('index') }}">{{ __('Index') }}</a>
+        <a class="button" href="{{ route('register') }}">{{ __('Register') }}</a>
+        <a id="forgot_your_password" class="button" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
     </form>
 @endsection
