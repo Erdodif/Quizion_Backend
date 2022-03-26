b02361a23a0db860b73f3acb5cacd6f28781f830
Nagy Bendegúz <bendeguz@medea.hu>
2022-03-26 21:23:20 +0100

    Web - leaderboard.js refactor.

--- resources/js/leaderboard.js
+++ resources/js/leaderboard.js
@@ -1,38 +1,23 @@
 
-function loadQuizHeader(id)
-{
-    return fetch(`${window.url}/api/quizzes/${id}`)
-    .then(function (response) {
-        return response.json();
-    });
-}
-
-async function loadLeaderboard(id)
-{
-    let response = await fetch(`${window.url}/api/leaderboard/${id}`);
-    let data = await response.json();
-    document.getElementById("leaderboard").innerHTML = "";
-    let rows = Object.keys(data).length;
-    let table = document.createElement("table");
-    createTableTh(table);
-    for (let i = 0; i < rows; i++) {
-        let tr = document.createElement("tr");
-        tr.appendChild(addColumn("td", data[i].rank));
-        tr.appendChild(addColumn("td", data[i].points));
-        tr.appendChild(addColumn("td", data[i].name));
-        if (window.username == data[i].name) {
-            tr.classList.add("leaderboard_my_name");
-        }
-        table.appendChild(tr);
+function loadTitle(id) {
+    if (sessionStorage.getItem("header")) {
+        document.getElementById("title").innerHTML = sessionStorage.getItem("header");
+        sessionStorage.removeItem("header");
     }
-    document.getElementById("leaderboard").appendChild(table);
+    else {
+        fetch(`${window.url}/api/quizzes/${id}`)
+        .then((response) => response.json())
+        .then((response) => {
+            document.getElementById("title").innerHTML = response.header;
+        });
+    }
 }
 
-function addColumn(htmlThTr, item)
-{
-    let htmlElement = document.createElement(htmlThTr);
-    htmlElement.innerHTML = item;
-    return htmlElement;
+function loadResult() {
+    if (sessionStorage.getItem("result")) {
+        document.getElementById("result").innerHTML = sessionStorage.getItem("result");
+        sessionStorage.removeItem("result");
+    }
 }
 
 function createTableTh(table)
@@ -44,22 +29,37 @@ function createTableTh(table)
     table.appendChild(trTh);
 }
 
+function addColumn(htmlThTr, item)
+{
+    let htmlElement = document.createElement(htmlThTr);
+    htmlElement.innerHTML = item;
+    return htmlElement;
+}
+
+async function loadLeaderboard(id)
+{
+    let response = await (await fetch(`${window.url}/api/leaderboard/${id}`)).json();
+    document.getElementById("leaderboard").innerHTML = "";
+    let rows = Object.keys(response).length;
+    let table = document.createElement("table");
+    createTableTh(table);
+    for (let i = 0; i < rows; i++) {
+        let tr = document.createElement("tr");
+        tr.appendChild(addColumn("td", response[i].rank));
+        tr.appendChild(addColumn("td", response[i].points));
+        tr.appendChild(addColumn("td", response[i].name));
+        if (window.username == response[i].name) {
+            tr.classList.add("leaderboard_my_name");
+        }
+        table.appendChild(tr);
+    }
+    document.getElementById("leaderboard").appendChild(table);
+}
+
 function init()
 {
-    if (sessionStorage.getItem("header")) {
-        document.getElementById("title").innerHTML = sessionStorage.getItem("header");
-        sessionStorage.removeItem("header");
-    }
-    else {
-        loadQuizHeader(window.quizId).then(function (response) {
-            (response) => response.json();
-            document.getElementById("title").innerHTML = response.header;
-        });
-    }
-    if (sessionStorage.getItem("result")) {
-        document.getElementById("result").innerHTML = sessionStorage.getItem("result");
-        sessionStorage.removeItem("result");
-    }
+    loadTitle(window.quizId);
+    loadResult();
     loadLeaderboard(window.quizId);
 }
 
