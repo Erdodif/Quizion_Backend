8022c3581fb3db32dd6f7d2a0340c8f8e2a47af4
SomaHam44 <somasolti@gmail.com>
2022-04-06 13:46:53 +0200

    QuizTest

--- tests/Unit/QuizTest.php
+++ tests/Unit/QuizTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Unit;
 
+use App\Http\Controllers\API\QuizAnswerController;
+use App\Http\Controllers\API\QuizController;
 use App\Http\Controllers\API\QuizQuestionController;
+use CreateQuizTable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+
 
 class QuizTest extends TestCase
 {
@@ -15,4 +19,17 @@ class QuizTest extends TestCase
         $this->seed();
         $this->assertEquals('{"count":2}', QuizQuestionController::getCountByQuiz(1)->toResponse()->content());
     }
+
+    public function test_first_quiz_count_question_warning()
+    {
+        $this->seed();
+        $this->assertNotEquals('{"count":3}', QuizQuestionController::getCountByQuiz(1)->toResponse()->content());
+    }
+
+    /*public function test_quiz()
+    {
+        $this->seed();
+        $this->assertEmpty(Quiz::class);
+    }
+    */
 }
