8fd0a8f1800832a47ba235f4f3bc965986b824e2
Erdodif <erdodif@gmail.com>
2022-04-06 13:08:30 +0200

    AdatBázis tesztosztály kiemelése, quizteszt különválasztása

--- tests/Unit/QuizTest.php
+++ tests/Unit/QuizTest.php
@@ -11,37 +11,8 @@ use Database\Seeders\QuizTableSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class QuizTest extends TestCase
+class QuizTest extends DataTestCase
 {
-    use RefreshDatabase;
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->seed();
-    }
-
-    private function responseAssertion(Data $response,int $responseCode = 200, string $responseString = null){
-        if($responseString !== null){
-            $this->assertEquals($responseString,$response->toResponse()->content());
-        }
-        $this->assertEquals($responseCode,$response->getCode());
-    }
-
-    public function test_response_assertion(){
-        $response = new Data(404,new Message("test"));
-        $this->responseAssertion($response,404,'{"message":"test"}');
-    }
-
-    public function test_response_assertion_empty_string(){
-        $response = new Data(300,new Message("test"));
-        $this->responseAssertion($response,300);
-    }
-
-    public function test_response_assertion_empty(){
-        $response = new Data(200,new Message("test"));
-        $this->responseAssertion($response);
-    }
-
     public function test_first_quiz_count_existing()
     {
         $response =  QuizQuestionController::getCountByQuiz(1);
