8456b6de9d7f8c6e8454419b75788c15f11798f7
Erdodif <erdodif@gmail.com>
2022-04-07 12:52:37 +0200

    Játék teszt előkészületek

--- tests/Unit/GameTest.php
+++ tests/Unit/GameTest.php
@@ -6,5 +6,8 @@ use Tests\Unit\Classes\AuthenticatedTestCase;
 
 class GameTest extends AuthenticatedTestCase
 {
-
+    public function testGameCreated(){
+        $response = $this->postWithToken("api/play/newgame/1");
+        $this->testResponseAssertion($response,201);
+    }
 }
