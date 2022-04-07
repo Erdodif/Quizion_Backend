5b103924c96bb7cb035f08681cacaeb98a77ea8f
Erdodif <erdodif@gmail.com>
2022-04-07 15:26:18 +0200

    Teszt már létező játékmenetre

--- tests/Unit/GameTest.php
+++ tests/Unit/GameTest.php
@@ -2,12 +2,23 @@
 
 namespace Tests\Unit;
 
+use App\Models\Game;
 use Tests\Unit\Classes\AuthenticatedTestCase;
 
 class GameTest extends AuthenticatedTestCase
 {
-    public function testGameCreated(){
-        $response = $this->postWithToken("api/play/newgame/1");
+    public function setUp():void{
+        parent::setUp();
+        Game::addNew(["user_id"=>6,"quiz_id"=>1]);
+    }
+
+    public function test_game_created(){
+        $response = $this->postWithToken("api/play/newgame/2");
         $this->testResponseAssertion($response,201);
+    }
+
+    public function test_game_create_exist(){
+        $response = $this->postWithToken("api/play/newgame/1");
+        $this->testResponseAssertion($response,500);
     }
 }
