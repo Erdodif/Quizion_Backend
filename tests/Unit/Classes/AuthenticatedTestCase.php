188f1536a60afc5e76739a72d05dbd7b2a8c7f77
Erdodif <erdodif@gmail.com>
2022-04-07 12:52:19 +0200

    javítás

--- tests/Unit/Classes/AuthenticatedTestCase.php
+++ tests/Unit/Classes/AuthenticatedTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Classes;
 
+use Database\Seeders\DatabaseSeeder;
 use Tests\Unit\Classes\DataTestCase;
 
 class AuthenticatedTestCase extends DataTestCase
@@ -9,16 +10,15 @@ class AuthenticatedTestCase extends DataTestCase
     protected $token;
     public function setUp(): void
     {
-        parent::setUp();
+        parent::setUp(DatabaseSeeder::class);
         $this->token = $this->post("/api/users/login", [
             "userID" => "test",
-            "password" => "test",
-            "remember" => false
+            "password" => "test"
         ])->json("token");
     }
 
     public function postWithToken($uri, array $data = [], array $headers = [])
     {
-        return $this->post($uri,$data,array_merge($headers,["Authentication"=>"bearer "+$this->token]));
+        return $this->post($uri,$data,array_merge($headers,["Authorization"=>"Bearer ".$this->token, "Accept"=>"application/json"]));
     }
 }
