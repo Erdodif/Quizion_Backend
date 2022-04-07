91bc5e059764198dc94ba1e4eda847be0a56a3a7
Erdodif <erdodif@gmail.com>
2022-04-07 16:27:33 +0200

    Javítás

--- tests/Unit/Classes/AuthenticatedTestCase.php
+++ tests/Unit/Classes/AuthenticatedTestCase.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Classes;
 
 use Database\Seeders\DatabaseSeeder;
+use Illuminate\Testing\TestResponse;
 use Tests\Unit\Classes\DataTestCase;
 
 class AuthenticatedTestCase extends DataTestCase
@@ -11,19 +12,24 @@ class AuthenticatedTestCase extends DataTestCase
     public function setUp(): void
     {
         parent::setUp(DatabaseSeeder::class);
+        $this->refreshToken();
+    }
+
+    public function refreshToken()
+    {
         $this->token = $this->post("/api/users/login", [
             "userID" => "test",
             "password" => "test"
         ])->json("token");
     }
 
-    public function postWithToken($uri, array $data = [], array $headers = [])
+    public function postWithToken($uri, array $data = []): TestResponse
     {
-        return $this->post($uri,$data,array_merge($headers,["Authorization"=>"Bearer ".$this->token, "Accept"=>"application/json"]));
+        return $this->post($uri, $data, ["Authorization" => "Bearer " . $this->token, "Accept" => "application/json"]);
     }
 
-    public function getWithToken($uri, array $data = [], array $headers = [])
+    public function getWithToken($uri): TestResponse
     {
-        return $this->get($uri,$data,array_merge($headers,["Authorization"=>"Bearer ".$this->token, "Accept"=>"application/json"]));
+        return $this->get($uri, ["Authorization" => "Bearer " . $this->token, "Accept" => "application/json"]);
     }
 }
